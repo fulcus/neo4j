61a0f54844a0582bbaad4d3e310239108d51929d
fulcus <francescofgonzales@gmail.com>
2021-11-03 19:02:47 +0100

    created visited

--- lives.py
+++ lives.py
@@ -28,24 +28,16 @@ with open(filename, 'r') as csvfile:
 # printing the field names
 print('Field names are:' + ', '.join(field for field in fields) + '\n')
 
-# printing first 5 rows
-# print('\nFirst 5 rows are:\n')
-# for row in rows[:5]:
-# 	# parsing each column of a row
-# 	for col in row:
-# 		print("%10s"%col),
-# 	print('\n')
-
 # Field names are: 0: first_name, 1: last_name, 2: email, 3: cf
 
 
-def randomSublists(someList):
+def randomSublists(someList, num_families=80, max_size_families=6):
     resultList = [] #result container
     index = 0 #start at the start of the list
     #length = len(someList) #and cache the length for performance on large lists
-    length = 80 # number of families
+    length = num_families # number of families
     while (index < length):
-        randomNumber = random.randint(2, 6) #get a number between 1 and the remaining choices
+        randomNumber = random.randint(2, max_size_families) #get a number between 1 and the remaining choices
         resultList.append(someList[index:index+randomNumber]) #append a list starting at index with randomNumber length to it
         index = index + randomNumber #increment index by amount of list used
     return resultList #return the list of randomized sublists
@@ -59,14 +51,11 @@ def lives_with():
     '''
     f = open('rel/lives_with.txt', 'w')
 
-    families = randomSublists(rows)
+    families = randomSublists(rows, num_families=80, max_size_families=2)
     for family in families:
         for pair in itertools.combinations(family, 2):
             # print(pair)
             s = "MATCH (a:Person), (b:Person) WHERE a.cf = '" + pair[0][3] + "' and b.cf = '" + pair[1][3] + "' CREATE (a)-[r: LIVES_WITH]-(b)\n"
             f.write(s)
-
-
-
 
 
