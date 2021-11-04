80ba88e7c6c2639ba142f25127ce77d69b9de125
fulco <fulco@MacBook-Air-di-Francesca-2.local>
2021-11-04 22:05:07 +0100

    updated lives to remove redundant symmetric, created tested and vaccinated

---
+++ vaccinated.py
@@ -0,0 +1,48 @@
+# importing csv module
+import csv
+import random
+
+ 
+def read_csv(filename):
+    # initializing the titles and rows list
+    fields = []
+    rows = []
+
+    # reading csv file
+    with open(filename, 'r') as csvfile:
+        # creating a csv reader object
+        csvreader = csv.reader(csvfile)
+        
+        # extracting field names through first row
+        fields = next(csvreader)
+
+        # extracting each data row one by one
+        for row in csvreader:
+            rows.append(row)
+
+        # get total number of rows
+        print("Total no. of rows: %d"%(csvreader.line_num))
+
+    # printing the field names
+    print('Field names are:' + ', '.join(field for field in fields) + '\n')
+    return rows
+
+# Field names are: 0: first_name, 1: last_name, 2: email, 3: cf
+
+
+def vaccinated_with(num_vaccines):
+    '''
+    MATCH (a:Person), (v:Vaccine) WHERE a.cf = '1' and v.type = 'Pfizer'  
+    CREATE (a) - [r:VACCINATED_WITH] -> (v)
+    '''
+    persons = read_csv('csv/people.csv')
+    vaccines = read_csv('csv/vaccines.csv')
+    f = open('rel/vaccinated.txt', 'w')
+
+    for i in range(num_vaccines):
+        cf = random.choice(persons)[3]
+        id = random.choice(vaccines)[0]
+        s = f'CREATE (:Person{{cf: {cf}}})-[VACCINATED_WITH]->(:Vaccine{{id: {id}}})\n'
+        f.write(s)
+
+vaccinated_with(10)
