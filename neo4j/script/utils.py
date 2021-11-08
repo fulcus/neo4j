7fd06dc76254b32783d8269accaf010f85d03fd7
fulcus <francescofgonzales@gmail.com>
2021-11-08 13:13:50 +0100

    changed directory structure

--- neo4j/script/utils.py
+++ neo4j/script/utils.py
@@ -1,8 +1,6 @@
-# importing csv module
+import os
 import csv
-import random
 
- 
 def read_csv(filename):
     # initializing the titles and rows list
     fields = []
@@ -26,23 +24,3 @@ def read_csv(filename):
     # printing the field names
     print('Field names are:' + ', '.join(field for field in fields) + '\n')
     return rows
-
-# Field names are: 0: first_name, 1: last_name, 2: email, 3: cf
-
-
-def tested_with(num_tests):
-    '''
-    MATCH (a:Person), (t:Test) WHERE a.cf = '1' and t.id = '1'  
-    CREATE (a)-[r:TESTED_WITH] -> (t)
-    '''
-    persons = read_csv('csv/people.csv')
-    tests = read_csv('csv/tests.csv')
-    f = open('rel/tested.txt', 'w')
-
-    for i in range(num_tests):
-        cf = random.choice(persons)[3]
-        id = random.choice(tests)[0]
-        s = "MATCH (a:Person{cf:'" + cf +"'}), (b:Test{id:'" + id + "'}) CREATE (a)-{:TESTED_WITH]->(b);\n"
-        f.write(s)
-
-tested_with(10)
--- neo4j/script/visited.py
+++ neo4j/script/visited.py
@@ -1,33 +1,6 @@
-# importing csv module
-import csv
+import os
 import random
-
- 
-def read_csv(filename):
-    # initializing the titles and rows list
-    fields = []
-    rows = []
-
-    # reading csv file
-    with open(filename, 'r') as csvfile:
-        # creating a csv reader object
-        csvreader = csv.reader(csvfile)
-        
-        # extracting field names through first row
-        fields = next(csvreader)
-
-        # extracting each data row one by one
-        for row in csvreader:
-            rows.append(row)
-
-        # get total number of rows
-        print("Total no. of rows: %d"%(csvreader.line_num))
-
-    # printing the field names
-    print('Field names are:' + ', '.join(field for field in fields) + '\n')
-    return rows
-
-# Field names are: 0: first_name, 1: last_name, 2: email, 3: cf
+from utils import read_csv
 
 
 # todo: format date properly, check same person not in same place at near times
@@ -36,9 +9,9 @@ def visited(num_visits):
     MATCH (a:Person), (p:Place) WHERE a.cf = '1' and p.id = '1' 
     CREATE (a)-[r: VISITED {date: datetime('2015-06-24T12:50:35.556+0100')}]->(p)
     '''
-    persons = read_csv('csv/people.csv')
-    places = read_csv('csv/places.csv')
-    f = open('rel/visited.txt', 'w')
+    persons = read_csv(os.getcwd() + '/csv/people.csv')
+    places = read_csv(os.getcwd() + '/csv/places.csv')
+    f = open(os.getcwd() + '/rel/visited.txt', 'w')
 
     # randomly sample datetime
     import datetime
