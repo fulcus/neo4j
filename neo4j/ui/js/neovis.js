6eb8384df495ec2b58edfa48b37b2e654e1b2fde
Ginevra Iorio <ginevra1.iorio@mail.polimi.it>
2021-11-13 12:54:38 +0100

    started ui queries and commands

--- neo4j/ui/js/neovis.js
+++ neo4j/ui/js/neovis.js
@@ -6,12 +6,12 @@ var viz;
 
 function draw() {
     var config = {
-        encrypted: "ENCRYPTION_ON",
-        trust: "TRUST_SYSTEM_CA_SIGNED_CERTIFICATES",
+        //encrypted: "ENCRYPTION_ON",
+        //trust: "TRUST_SYSTEM_CA_SIGNED_CERTIFICATES",
         container_id: "viz",
-        server_url: "bolt://6e1205c9.databases.neo4j.io",
+        server_url: "neo4j://localhost:7687",
         server_user: "neo4j",
-        server_password: "Sewg53RImYM_5SAI0QA4DYOs7iOsHIMVcSmgCe9YuSE",
+        server_password: "neo4j",
         labels: {
             //"Character": "name",
             "Character": {
@@ -30,8 +30,73 @@ function draw() {
         initial_cypher: "MATCH (n) RETURN n"
     };
 
+    node.style.display="none";
+    rel.style.display="none";
+
+    
+    person_display.style.display="none";
+    place_display.style.display="none";
+    test_display.style.display="none";
+    vaccine_display.style.display="none";
+
     viz = new NeoVis.default(config);
     viz.render();
     console.log(viz);
 
+}
+
+function showTypeChange(){
+
+    switch(document.getElementById("command_type").value){
+        case 'node':
+            rel.style.display="none";
+            node.style.display="block";
+            break;
+
+        case 'relationship':
+            node.style.display="none";    
+            rel.style.display="block";
+            break;
+        }
+}
+
+
+function showNodeChange(){
+    var node_type = document.getElementById("node_type").value;
+
+    switch(node_type){
+        case 'person': 
+            place_display.style.display="none";
+            test_display.style.display="none";
+            vaccine_display.style.display="none";
+            person_display.style.display="block";
+
+            break;
+
+        case 'place':
+            person_display.style.display="none";
+            test_display.style.display="none";
+            vaccine_display.style.display="none";
+            place_display.style.display="block";
+            break;
+
+        case 'test':
+            person_display.style.display="none";
+            place_display.style.display="none";
+            vaccine_display.style.display="none";
+            test_display.style.display="block";
+            break;
+
+        case 'vaccine':
+            person_display.style.display="none";
+            place_display.style.display="none";
+            test_display.style.display="none";
+            vaccine_display.style.display="block";
+            break;
+
+    }
+}
+
+function showRelChange(){
+        var rel_type = document.getElementById("rel_type").value;
 }
